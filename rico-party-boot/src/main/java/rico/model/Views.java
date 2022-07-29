38842ff6292264a7ab5a8e395abafd8487f55caa
Cassandre <certaincassandre@gmail.com>
2022-07-29 11:16:35 +0200

    views

---
+++ rico-party-boot/src/main/java/rico/model/Views.java
@@ -0,0 +1,32 @@
+package rico.model;
+
+public class Views {
+	public static class ViewCommon {
+	}
+
+	public static class ViewAdresse extends ViewCommon {
+	}
+
+	public static class ViewContribution extends ViewCommon {
+	}
+	
+	public static class ViewEvent extends ViewCommon {
+	}
+	
+	public static class ViewMessage extends ViewCommon {
+	}
+	
+	public static class ViewParticipation extends ViewCommon {
+	}
+	
+	public static class ViewCompte extends ViewCommon {
+	}
+	
+	public static class ViewAdmin extends ViewCompte {
+	}
+	
+	public static class ViewUser extends ViewCompte {
+	}
+
+
+}
