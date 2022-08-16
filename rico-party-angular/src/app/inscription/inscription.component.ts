663a6a57476ea23e93578a5c32f8ef10522a8c4b
bilelbouj <bilel.boujdaria@gmail.com>
2022-08-16 09:18:21 +0200

    connexion + signup

--- rico-party-angular/src/app/inscription/inscription.component.ts
+++ rico-party-angular/src/app/inscription/inscription.component.ts
@@ -1,15 +1,28 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { InscriptionHttpService } from './inscription-http.service';
 
 @Component({
   selector: 'app-inscription',
   templateUrl: './inscription.component.html',
   styleUrls: ['./inscription.component.css']
 })
-export class InscriptionComponent implements OnInit {
+export class inscriptionComponent implements OnInit {
+  public InscriptionForm !: FormGroup;
+  signupForm: any;
 
-  constructor() { }
-
+  constructor(private formBuilder : FormBuilder, private http : HttpClient) {}
   ngOnInit(): void {
+    this.InscriptionForm = this.formBuilder.group({
+      nom : [''],
+      email: [''],
+      password : ['']
+     
+    })
+  }
+  inscri(){
+    this.http.post<any>()
   }
 
 }
